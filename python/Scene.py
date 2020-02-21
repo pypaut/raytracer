1fafba381929fb3672ff444640478cca4cde7bf9
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-02-21 10:52:26 +0100

    Add Scene class.

---
+++ python/Scene.py
@@ -0,0 +1,8 @@
+class Scene:
+    def __init__(self, objects, lights, cam):
+        self.objects = objects
+        self.lights = lights
+        self.cam = cam
+
+    def buildImage(self):
+        pass
