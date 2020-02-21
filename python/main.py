894d5d61846475151ef86b670e10d5bd635fd8eb
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-02-21 11:15:36 +0100

    Use main class.

---
+++ python/main.py
@@ -0,0 +1,17 @@
+from Image import Image
+
+def main():
+    W = 600
+    H = 600
+    pixels = []
+    for i in range(W):
+        line = []
+        for j in range(H):
+            line.append((0, 0, 0,))
+        pixels.append(line)
+
+    im = Image(W, H, pixels)
+    im.export_ppm("result.ppm")
+
+if __name__ == "__main__":
+    main()
