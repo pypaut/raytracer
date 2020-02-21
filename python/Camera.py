5bc138abaadfc15b3b8c8ff724deeb9cef71002f
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-02-21 13:18:03 +0100

    Camera : enhance + compute right vector.

--- python/Camera.py
+++ python/Camera.py
@@ -9,7 +9,9 @@ class Camera:
         float    z_min  : image plan distance to self
         """
         self.pos = pos
-        self.fwd = fwd
-        self.up = up
+        self.fwd = fwd.normalized()
+        self.up = up.normalized()
+        self.right = self.fwd.cross(self.up).normalized()
         self.rf_x = rf_x
         self.rf_y = rf_y
+        self.z_min = z_min
