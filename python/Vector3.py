5c0dc6e24d811f968cbe84f82402778bb63fbd7a
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-02-21 09:56:07 +0100

    Add Vector3 class.

---
+++ python/Vector3.py
@@ -0,0 +1,44 @@
+class Vector3:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        """
+        Print overload.
+        """
+        return f"Vector3({self.x},{self.y},{self.z})"
+
+    def __add__(self, vec):
+        """
+        Sum overload.
+        """
+        x = self.x + vec.x
+        y = self.y + vec.y
+        z = self.z + vec.z
+        return Vector3(x, y, z)
+
+    def __sub__(self, vec):
+        """
+        Substraction overload.
+        """
+        x = self.x - vec.x
+        y = self.y - vec.y
+        z = self.z - vec.z
+        return Vector3(x, y, z)
+
+    def dot(self, vec):
+        """
+        Dot product.
+        """
+        return self.x * vec.x + self.y * vec.y + self.z * vec.z
+
+    def times(self, t):
+        """
+        Product with a scalar.
+        """
+        x = t * self.x
+        y = t * self.y
+        z = t * self.z
+        return Vector3(x, y, z)
