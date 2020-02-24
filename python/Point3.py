b6f4eb0d46a846933f5129dced8eee4a9b9c54ca
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-02-24 23:59:54 +0100

    Point3.dist(Point3) method.

--- python/Point3.py
+++ python/Point3.py
@@ -1,3 +1,6 @@
+from Vector3 import Vector3
+
+
 class Point3:
     def __init__(self, x=0, y=0, z=0):
         self.x = x
@@ -6,3 +9,11 @@ class Point3:
 
     def __str__(self):
         return f"Point3({self.x},{self.y},{self.z})"
+
+    def dist(self, pt):
+        vec = Vector3(
+            self.x - pt.x,
+            self.y - pt.y,
+            self.z - pt.z
+        )
+        return vec.norm()
