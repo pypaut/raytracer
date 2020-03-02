8608db327c506d51a7f781ed4c97ce6547e31425
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-03-02 11:34:29 +0100

    Refactor Raytracer.

--- python/Raytracer.py
+++ python/Raytracer.py
@@ -5,7 +5,51 @@ from Image import Image
 
 class Raytracer:
     @classmethod
+    def computePixPos(self, i, j, pixSize, screenUpLeft, cam):
+        pixPos = (
+            screenUpLeft
+            + cam.right.times(i * pixSize)
+            - cam.up.times(j * pixSize)
+        )
+        pixPos += cam.right.times(pixSize / 2) - cam.up.times(pixSize / 2)
+        return pixPos
+
+    @classmethod
+    def ptIsAfterScreen(self, pt, pixPos, cam):
+        objDir = Vector3.buildDir(pixPos, pt)
+        dir_cond = pt and cam.fwd.dot(objDir) >= 0
+        pos_cond = cam.pos.dist(pt) >= cam.pos.dist(pixPos)
+        return dir_cond and pos_cond
+
+    @classmethod
+    def applyDiffuseComp(self, color, diffuseComp):
+        return (
+            color[0] * diffuseComp,
+            color[1] * diffuseComp,
+            color[2] * diffuseComp,
+        )
+
+    @classmethod
+    def computeDiffuse(self, obj, pt, lights):
+        """
+        IMPORTANT
+        Everything normalized, ending up with a [0,1]
+        coefficient. This will efficiently scale the
+        rendered color.
+        """
+        diffuseComp = 0
+        for light in lights:
+            L = Vector3.buildDir(pt, light.pos).normalized()
+            obj.k_d = 1  # TODO
+            add = obj.k_d * obj.normal(pt).dot(L) * light.intensity(pt)
+            if add < 0:
+                add = 0
+            diffuseComp += add
+        return diffuseComp
+
+    @classmethod
     def buildImage(self, scene, pixSize):
+        # Compute needed parameters
         cam = scene.cam
         vecCamPos = Vector3(cam.pos.x, cam.pos.y, cam.pos.z)
         screenCenter = vecCamPos + cam.fwd.times(cam.z_min)
@@ -21,63 +65,31 @@ class Raytracer:
         nbPixH = int(height // pixSize)
         nbPixW = int(width // pixSize)
 
-        # Pixels
+        # Pixel matrix
         pixels = []
 
         for i in range(nbPixH):
             line = []
             for j in range(nbPixW):
+                pixPos = self.computePixPos(i, j, pixSize, screenUpLeft, cam)
+                rayDir = Vector3.buildDir(cam.pos, pixPos).normalized()
                 color = (0, 0, 0)
                 dist_min = -1
-                pixPos = (
-                    screenUpLeft
-                    + cam.right.times(i * pixSize)
-                    - cam.up.times(j * pixSize)
-                )
-                pixPos += cam.right.times(pixSize / 2) - cam.up.times(
-                    pixSize / 2
-                )
-                rayDir = Vector3.buildDir(cam.pos, pixPos).normalized()
-                # Color
                 for obj in scene.objects:
                     pt = obj.intersects(pixPos, rayDir)
                     if not pt:
                         continue
-                    # Check if object is after the screen
-                    objDir = Vector3.buildDir(pixPos, pt)
-                    dir_cond = pt and cam.fwd.dot(objDir) >= 0
-                    pos_cond = cam.pos.dist(pt) >= cam.pos.dist(pixPos)
-                    if dir_cond and pos_cond:
+                    if self.ptIsAfterScreen(pt, pixPos, cam):
                         dist = pt.dist(pixPos)
                         if (
                             dist_min == -1 or dist < dist_min
                         ):  # This point is closer
                             dist_min = dist
                             color = obj.color
-                            # Diffuse component
-                            diffuseComp = 0
-                            for light in scene.lights:
-                                # IMPORTANT
-                                # Everything normalized, ending up with a [0,1]
-                                # coefficient. This will efficiently scale the
-                                # rendered color.
-                                L = Vector3.buildDir(
-                                    pt, light.pos
-                                ).normalized()
-                                obj.k_d = 1
-                                add = (
-                                    obj.k_d
-                                    * obj.normal(pt).dot(L)
-                                    * light.intensity(pt)
-                                )
-                                if add < 0:
-                                    add = 0
-                                diffuseComp += add
-                            color = (
-                                color[0] * diffuseComp,
-                                color[1] * diffuseComp,
-                                color[2] * diffuseComp,
+                            diffuseComp = self.computeDiffuse(
+                                obj, pt, scene.lights
                             )
+                            color = self.applyDiffuseComp(color, diffuseComp)
 
                 color = (int(color[0]), int(color[1]), int(color[2]))
                 line.append(color)
